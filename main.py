5446efb48c73443aaa2c1913153766fea8dc5cff
ddeiro <ddeiro@bdsdatasolution.com.br>
2025-01-03 13:29:20 -0300

    feat: Update stages to ETL

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ logfire.install_auto_tracing(modules=['src'], min_duration=0.01)
 logfire.instrument_system_metrics()
 
 from src.extract import get_br_gov_data
-from src.transform import clean_dataframe, rename_columns_to_english
+from src.transform import clean_dataframe, rename_columns_to_english, define_columns_dtypes
+from src.data_contract import schema_validator
 
-if __name__ == '__main__':
+df_raw = get_br_gov_data()
 
-    df_raw = get_br_gov_data()
-    
-    df_cleanned = clean_dataframe(df_raw)
-    
-    df_english = rename_columns_to_english(df_cleanned)
-    
-    print(df_english.dtypes)
+df_cleanned = clean_dataframe(df_raw)
+
+df_english = rename_columns_to_english(df_cleanned)
+
+df_final = define_columns_dtypes(df_english)
+
+validated_df = schema_validator(df_final)
+
+validated_df.to_csv('brazilian_tax_revenue.csv', index=False, sep=';')
