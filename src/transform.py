2fe6527a63da4857174bf7574d5d89ad1a9efa5b
ddeiro <ddeiro@bdsdatasolution.com.br>
2025-01-03 13:27:23 -0300

    feat: Update how to clean dataframe

--- src/transform.py
+++ src/transform.py
@@ -1,8 +1,36 @@
-from src.constants import columns_from_to_dict
+from src.constants import columns_from_to_dict, english_months
 import pandas as pd
 import logfire
 
 logfire = logfire.configure()
+
+def identify_cents_in_number(value: str) -> float:
+    """
+    This function identifies if a float column has cents.
+    
+    Parameters
+    ----------
+    value : float
+    The value to be checked.
+    
+    Returns
+    float
+    The value with cents or not.
+    """
+    
+    try:
+        cleanned_value = value.replace('.', '')
+        full_value = cleanned_value.split(',')
+        last_value = full_value[-1]
+        if len(last_value) == 2:
+            correct_value = float(''.join(full_value[:-1]) + '.' + last_value)
+        else:
+            correct_value = int(value.replace(',', '').replace('.', ''))
+            
+        return correct_value
+    except:
+        value = value
+        return value    
 
 @logfire.instrument(span_name='transform_brazilian_tax_revenue_data')
 def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
@@ -20,8 +48,19 @@ def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
     df = df.drop_duplicates()
     
-    logfire.info("Dropped duplicates.", context={"rows": len(df)})    
+    df = df.dropna(how='all')
+    
+    logfire.info("Dropped duplicates.", context={"rows": len(df)})
+    
+    for column in df.columns:
+        if column in ['Month', 'State']:
+            df[column] = df[column].str.strip()
+        else:
+            df[column] = df[column].fillna(0)
+            df[column] = df[column].apply(lambda x: identify_cents_in_number(x))    
+    logfire.info("Stripped strings and filled NaN values.", context={"columns": list(df.columns)})
     return df
+
 
 @logfire.instrument(span_name='transform_brazilian_tax_revenue_data')
 def rename_columns_to_english(df: pd.DataFrame) -> pd.DataFrame:
@@ -39,6 +78,8 @@ def rename_columns_to_english(df: pd.DataFrame) -> pd.DataFrame:
     """
     df = df.rename(columns=columns_from_to_dict)
     logfire.info("Columns renamed.", context={"columns": list(df.columns)})
+    df['Month'] = df['Month'].map(english_months)
+    logfire.info("Months translated.", context={"months": list(df['Month'].unique())})
     return df
 
 
@@ -67,11 +108,15 @@ def define_columns_dtypes(df: pd.DataFrame) -> pd.DataFrame:
                        'Withholding at Source - Law 10.833, Art. 30',
                        'Unified Payment']
     
-    for column in df.columns():
+    for column in df.columns:
         if column in int_column_list:
             df[column] = df[column].astype('int')
         elif column in ['Month', 'State']:
             df[column] = df[column].astype('object')        
         else:
             df[column] = df[column].astype('float')
+    
+    logfire.info("Columns data types defined.", context={"columns": list(df.columns)})
+    
+    return df
     
